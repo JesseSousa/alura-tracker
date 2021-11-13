477a4e7da284f3d753a1007f40216a627d3f6236
Jesse Sousa <rhadamesjesse14rj@gmail.com>
2021-11-13 10:56:48 -0300

    first commit

---
+++ src/components/Botao.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="button" @click="aoClique" :disabled="desabilitado">
+    <span class="icon">
+      <i :class="'fas fa-' + icone"></i>
+    </span>
+    <span>{{ texto }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Botao',
+  emits: ['botaoClicado'],
+  props: {
+    icone: {
+      type: String,
+      required: true,
+    },
+    texto: {
+      type: String,
+      required: true,
+    },
+    desabilitado: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    aoClique() {
+      this.$emit('botaoClicado');
+    },
+  },
+});
+</script>
